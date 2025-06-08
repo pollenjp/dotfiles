5655ed935dc1935e6ebc2e6ad502f9148ec63573
pollenjp <polleninjp@gmail.com>
2025-06-08 13:08:26 +0900

    support for 1password ssh-agent

--- shell/post_99_ssh_agent.sh
+++ shell/post_99_ssh_agent.sh
@@ -3,8 +3,19 @@
 # ssh-agent
 # <https://himadatanode.hatenablog.com/entry/20160823/p14>
 SSH_AGENT_FILE="${HOME}"/.ssh-agent
-# shellcheck disable=SC1090
-test -f "$SSH_AGENT_FILE" && source "${SSH_AGENT_FILE}"
+
+# If already ssh-agent is running, use it (do nothing).
+# ex) 1password ssh-agent
+
+# If ssh-agent has not been started
+# and SSH_AGENT_FILE exists, load it.
+# (Its agent may not exist anymore)
+if ! ssh-add -l >/dev/null 2>&1 && test -f "$SSH_AGENT_FILE"; then
+  # shellcheck disable=SC1090
+  source "${SSH_AGENT_FILE}"
+fi
+
+# If the ssh-agent is not working, start it and add the key.
 if ! ssh-add -l >/dev/null 2>&1; then
   ssh-agent >"${SSH_AGENT_FILE}"
   # shellcheck disable=SC1090
