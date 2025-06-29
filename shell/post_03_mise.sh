584fc7ce28d8e13034066467eab665d2c5f5f7ba
pollenjp <polleninjp@gmail.com>
2025-06-29 13:17:38 +0900

    [shell/post_03_mise.sh]Add alternative linux commands

---
+++ shell/post_03_mise.sh
@@ -0,0 +1,27 @@
+# shellcheck shell=bash
+
+########################
+# Alternative Commands #
+########################
+
+if command -v mise &>/dev/null; then
+  pkgs=(
+    # bat command (cat alternative)
+    cargo:bat
+    # fd command (fd-find) (find alternative)
+    cargo:fd-find
+    # exa command (ls alternative)
+    cargo:exa
+    # procs command (ps alternative)
+    cargo:procs
+    # rg command (ripgrep) (grep alternative)
+    cargo:ripgrep
+  )
+  #shellcheck disable=2016
+  mise_use_script='mise use -g "$1" &>/dev/null'
+  printf "%s\n" "${pkgs[@]}" | xargs -P0 -I{} bash -c "$mise_use_script" _ {}
+
+  if command -v bat &>/dev/null; then alias cat='bat'; fi
+  if command -v exa &>/dev/null; then alias ls='exa'; fi
+  if command -v rg &>/dev/null; then alias grep='rg'; fi
+fi
