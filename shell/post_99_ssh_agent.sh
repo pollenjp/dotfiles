15752acb0b01e5111c2b8680d5be3cc42ee800cf
pollenjp <polleninjp@gmail.com>
2025-12-02 21:31:55 +0900

    fix: strict check for existing ssh-agent

--- shell/post_99_ssh_agent.sh
+++ shell/post_99_ssh_agent.sh
@@ -17,9 +17,21 @@ fi
 
 # If the ssh-agent is not working, start it and add the key.
 if ! ssh-add -l >/dev/null 2>&1; then
-  ssh-agent >"${SSH_AGENT_FILE}"
-  # shellcheck disable=SC1090
-  source "${SSH_AGENT_FILE}"
+  # SSH_AGENT_PID に格納されている pid が ssh-agent であるか確認する
+  current_user=$(id -un)
+  if [[ -n "${SSH_AGENT_PID:-}" ]] \
+    && ps_out=$(ps -p "${SSH_AGENT_PID:?}" -o user=,comm=) \
+    && ps_user=$(awk '{print $1}' <<<"${ps_out}") \
+    && [[ "${ps_user}" == "${current_user}" ]] \
+    && ps_cmd=$(awk '{print $2}' <<<"${ps_out}") \
+    && [[ "${ps_cmd}" == "ssh-agent" ]] \
+  ; then
+    echo "ssh-agent is already running"
+  else
+    ssh-agent >"${SSH_AGENT_FILE}"
+    # shellcheck disable=SC1090
+    source "${SSH_AGENT_FILE}"
+  fi
 
   if [ -f "${HOME}"/.ssh/id_ed25519 ]; then
     ssh-add "${HOME}"/.ssh/id_ed25519
