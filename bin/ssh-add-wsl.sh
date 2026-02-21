49afa47c8ee70b2ae88e962b88a60400f028d40a
pollenjp <polleninjp@gmail.com>
2026-02-21 22:55:25 +0900

    add USE_LINUX_SSH for ssh-wsl

--- bin/ssh-add-wsl.sh
+++ bin/ssh-add-wsl.sh
@@ -2,4 +2,9 @@
 # wrapper for using 1password ssh client on WSL
 set -eu -o pipefail
 
-ssh-add.exe "$@"
+use_linux_ssh="${USE_LINUX_SSH:-"0"}"
+if [[ "${use_linux_ssh}" == "0" ]]; then
+  exec ssh-add.exe "$@"
+else
+  exec /usr/bin/ssh-add "$@"
+fi
