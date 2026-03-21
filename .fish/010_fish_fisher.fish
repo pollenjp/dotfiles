7e00377f630c9f72275668e390c2b6bb87d2460d
pollenjp <polleninjp@gmail.com>
2026-03-21 22:57:09 +0900

    feat: fzf_configure_bindings

--- .fish/010_fish_fisher.fish
+++ .fish/010_fish_fisher.fish
@@ -20,3 +20,21 @@ funcsave fish_greeting
 if not type -q fisher
   curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
 end
+
+if type -q fzf_configure_bindings
+  # fzf_configure_bindings --help
+  #
+  # COMMAND            |  DEFAULT KEY SEQUENCE         |  CORRESPONDING OPTION
+  # Search Directory   |  Ctrl+Alt+F (F for file)      |  --directory
+  # Search Git Log     |  Ctrl+Alt+L (L for log)       |  --git_log
+  # Search Git Status  |  Ctrl+Alt+S (S for status)    |  --git_status
+  # Search History     |  Ctrl+R     (R for reverse)   |  --history
+  # Search Processes   |  Ctrl+Alt+P (P for process)   |  --processes
+  # Search Variables   |  Ctrl+V     (V for variable)  |  --variables
+  #
+  fzf_configure_bindings \
+    --directory=alt-shift-f \
+    --git_log=alt-shift-l \
+    --git_status=alt-shift-s \
+    --processes=alt-shift-p
+end
