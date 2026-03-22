6792256abb98547ffc382107441578a0d43bd9a1
pollenjp <polleninjp@gmail.com>
2026-03-22 18:16:35 +0900

    feat: cdrepo - add command existence check

--- .fish/254_alias_fzf.fish
+++ .fish/254_alias_fzf.fish
@@ -1,5 +1,14 @@
 # ghq cd
 function cdrepo --description 'cd to ghq repository'
+  if not command -v ghq &>/dev/null
+    echo "ghq is not installed"
+    return 1
+  end
+  if not command -v fzf &>/dev/null
+    echo "fzf is not installed"
+    return 1
+  end
+
   set -l repodir (ghq list | fzf -1 +m) \
     && cd (ghq root)/$repodir
 end
--- shell/254_alias_fzf.sh
+++ shell/254_alias_fzf.sh
@@ -2,6 +2,15 @@
 
 # ghq cd
 function cdrepo() {
+  if not command -v ghq &>/dev/null; then
+    echo "ghq is not installed"
+    return 1
+  fi
+  if not command -v fzf &>/dev/null; then
+    echo "fzf is not installed"
+    return 1
+  fi
+
   local ghq_list repodir ghq_root
   ghq_list=$(ghq list) \
     && repodir=$(fzf -1 +m <<<"$ghq_list") \
